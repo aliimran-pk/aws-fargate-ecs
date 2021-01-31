3eebfbbf7770389638b1d76350f18a43a83472e8
Ali Imran <IMRAN@pk.ibm.com>
2021-01-31 13:08:58 +0500

    Added UserManagement Microservice

---
+++ usermanagement-service/src/main/java/com/ibm/usermanagement/core/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ibm.usermanagement.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ibm.usermanagement.core.entities.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+	public long countByEmail(String email);
+
+	@Query("select u.email from User u where u.username = ?1")
+	String findEmailByUsername(String username);
+}
